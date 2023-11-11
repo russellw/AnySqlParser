3f6277842bbdb06501ef567c62bb68317b45ce41
Russell Wallace <russell.wallace@gmail.com>
2023-11-11 07:07:38 +0000

    constructor

--- AnySqlParser/Parser.cs
+++ AnySqlParser/Parser.cs
@@ -1,7 +1,44 @@
 ﻿namespace AnySqlParser
 {
-    public class Parser
+    public sealed class Parser
     {
+        readonly string text;
+        int textIndex;
+        int token;
 
+        public Parser(string text)
+        {
+            this.text = text;
+            Lex();
+        }
+
+        void Lex()
+        {
+            while (textIndex < text.Length)
+            {
+                switch (text[textIndex])
+                {
+                    case '-':
+                        if (text[textIndex + 1] == '-')
+                        {
+                            textIndex = text.IndexOf('\n', textIndex + 2);
+                            if (textIndex < 0) textIndex = text.Length;
+                            continue;
+                        }
+                        break;
+                    case '\n':
+                    case '\r':
+                    case '\t':
+                    case '\f':
+                    case '\v':
+                    case ' ':
+                        textIndex++;
+                        continue;
+                }
+                token = text[textIndex++];
+                return;
+            }
+            token = -1;
+        }
     }
 }
