877041a6542428d62f461d240d4521b6aa1fa594
Russell Wallace <russell.wallace@gmail.com>
2023-11-11 21:21:31 +0000

    tests

--- AnySqlParserTest/UnitTest1.cs
+++ AnySqlParserTest/UnitTest1.cs
@@ -5,9 +5,27 @@ namespace AnySqlParserTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Blank()
         {
             Assert.Empty(Parser.Parse(""));
+            Assert.Empty(Parser.Parse("\t\n"));
+        }
+
+        [Fact]
+        public void LineComments()
+        {
+            Assert.Empty(Parser.Parse("--"));
+            Assert.Empty(Parser.Parse("--\n--\n"));
+        }
+
+        [Fact]
+        public void BlockComments()
+        {
+            Assert.Empty(Parser.Parse("/**/"));
+            Assert.Empty(Parser.Parse(" /*.*/ "));
+            Assert.Throws<FormatException>(()=> Parser.Parse("/*/"));
+            Assert.Empty(Parser.Parse("/**************/"));
+            Assert.Empty(Parser.Parse("/*////////////*/"));
         }
     }
 }
