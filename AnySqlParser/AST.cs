3fc5009a6cbaae31485b9bf69150975a9f4086b3
Russell Wallace <russell.wallace@gmail.com>
2023-11-15 23:55:33 +0000

    refactor

--- AnySqlParser/AST.cs
+++ AnySqlParser/AST.cs
@@ -51,35 +51,6 @@
         }
     }
 
-    public sealed class Column : AST
-    {
-        public string Name = null!;
-
-        //data type
-        public QualifiedName TypeName = null!;
-        public int Size = -1;
-        public int Scale = -1;
-
-        //etc
-        public bool Filestream;
-        public bool Sparse;
-
-        public bool Identity;
-        public int IdentitySeed = -1;
-        public int IdentityIncrement = -1;
-
-        public bool ForReplication = true;
-        public bool Nullable = true;
-        public bool Rowguidcol;
-
-        //constraints
-        public bool PrimaryKey;
-
-        public Column(Location location) : base(location)
-        {
-        }
-    }
-
     public sealed class If : AST
     {
         public Expression condition = null!;
@@ -177,16 +148,6 @@
         public string Value = null!;
 
         public SetParameter(Location location) : base(location)
-        {
-        }
-    }
-
-    public sealed class Table : AST
-    {
-        public QualifiedName Name = null!;
-        public List<Column> Columns = new();
-
-        public Table(Location location) : base(location)
         {
         }
     }
---
+++ AnySqlParser/Table.cs
@@ -0,0 +1,41 @@
+﻿namespace AnySqlParser
+{
+    public sealed class Column : AST
+    {
+        public string Name = null!;
+
+        //data type
+        public QualifiedName TypeName = null!;
+        public int Size = -1;
+        public int Scale = -1;
+
+        //etc
+        public bool Filestream;
+        public bool Sparse;
+
+        public bool Identity;
+        public int IdentitySeed = -1;
+        public int IdentityIncrement = -1;
+
+        public bool ForReplication = true;
+        public bool Nullable = true;
+        public bool Rowguidcol;
+
+        //constraints
+        public bool PrimaryKey;
+
+        public Column(Location location) : base(location)
+        {
+        }
+    }
+
+    public sealed class Table : AST
+    {
+        public QualifiedName Name = null!;
+        public List<Column> Columns = new();
+
+        public Table(Location location) : base(location)
+        {
+        }
+    }
+}
