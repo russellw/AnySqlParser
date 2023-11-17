5b4a3c8d172163d2b550e3058f1958c8b9bf41b1
Russell Wallace <russell.wallace@gmail.com>
2023-11-17 10:01:23 +0000

    rename

--- f.bat
+++ f.bat
@@ -1,4 +1,4 @@
 @echo off
 clang-format -i --style=file AnySqlParser\*.cs||exit /b
-clang-format -i --style=file AnySqlParserTest\*.cs||exit /b
+clang-format -i --style=file TestProject1\*.cs||exit /b
 git diff
