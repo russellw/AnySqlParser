7a9f808c8f935fc23e31568ba50950deca854a35
Russell Wallace <russell.wallace@gmail.com>
2023-11-22 01:20:40 +0000

    refactor

---
+++ AnySqlParser/ColumnOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace AnySqlParser {
+public sealed class ColumnOrder {
+	public readonly Location Location;
+
+	public string Name;
+	public bool Desc;
+
+	public ColumnOrder(Location location, string name) {
+		Location = location;
+		Name = name;
+	}
+}
+}
--- AnySqlParser/Statement.cs
+++ AnySqlParser/Statement.cs
@@ -1,16 +1,4 @@
 namespace AnySqlParser {
-public sealed class ColumnOrder {
-	public readonly Location Location;
-
-	public string Name;
-	public bool Desc;
-
-	public ColumnOrder(Location location, string name) {
-		Location = location;
-		Name = name;
-	}
-}
-
 public abstract class Statement {
 	public readonly Location Location;
 
