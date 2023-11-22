70e7b580e79b59bb8a78f76ea831e43522a5531c
Russell Wallace <russell.wallace@gmail.com>
2023-11-22 15:58:09 +0000

    reader

--- AnySqlParser/Parser.cs
+++ AnySqlParser/Parser.cs
@@ -1126,35 +1126,38 @@ public sealed class Parser {
 		}
 		case '"': {
 			var line1 = line;
+			Read();
 			var sb = new StringBuilder();
-			for (var i = textIndex + 1; i < text.Length;) {
-				switch (text[i]) {
+			for (;;) {
+				switch (ch) {
+				case -1:
+					throw Error("unclosed \"", line1);
 				case '\n':
 					line++;
 					break;
 				case '\\':
-					switch (text[i + 1]) {
+					switch (reader.Peek()) {
 					case '"':
 					case '\\':
-						sb.Append(text[i + 1]);
-						i += 2;
-						continue;
+						Read();
+						break;
 					}
 					break;
 				case '"':
-					if (text[i + 1] == '"') {
-						i += 2;
-						sb.Append('"');
-						continue;
+					Read();
+					switch (ch) {
+					case '"':
+						break;
+					default:
+						token = kQuotedName;
+						tokenString = sb.ToString();
+						return;
 					}
-					textIndex = i + 1;
-					token = kQuotedName;
-					tokenString = sb.ToString();
-					return;
+					break;
 				}
-				sb.Append(text[i++]);
+				sb.Append((char)ch);
+				Read();
 			}
-			throw Error("unclosed \"", line1);
 		}
 		case '`': {
 			var line1 = line;
