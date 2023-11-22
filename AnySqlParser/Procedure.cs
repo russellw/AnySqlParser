25468214ac0d054df935b2cc28424338cce420c1
Russell Wallace <russell.wallace@gmail.com>
2023-11-22 01:43:09 +0000

    procedure

---
+++ AnySqlParser/Procedure.cs
@@ -0,0 +1,15 @@
+﻿namespace AnySqlParser {
+public sealed class Procedure: Statement {
+	public bool OrAlter;
+	public QualifiedName Name = null!;
+	public int Number;
+	public List<string> Parameters = new();
+	public bool Encryption;
+	public bool Recompile;
+	public bool ForReplication;
+	public List<Statement> Body = new();
+
+	public Procedure(Location location): base(location) {
+	}
+}
+}
