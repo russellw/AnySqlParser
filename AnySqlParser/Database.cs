b59c2fc72dcc1563bfc174032ae85a10f7707fc1
Russell Wallace <russell.wallace@gmail.com>
2023-11-24 13:22:49 +0000

    database

---
+++ AnySqlParser/Database.cs
@@ -0,0 +1,17 @@
+﻿namespace AnySqlParser;
+public sealed class Database {
+	public List<Table> Tables = new();
+
+	public void Add(IEnumerable<Statement> statements) {
+		var adds = new List<AlterTableAdd>();
+		foreach (var statement in statements)
+			switch (statement) {
+			case AlterTableAdd add:
+				adds.Add(add);
+				break;
+			case Table table:
+				Tables.Add(table);
+				break;
+			}
+	}
+}
