6d6f8a0ab1102b7dae97841381e4e7071fb01eed
Russell Wallace <russell.wallace@gmail.com>
2023-12-03 22:11:24 +0000

    token

---
+++ AnySqlParser/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace AnySqlParser;
+public readonly struct Token {
+	public readonly Location Location;
+	public readonly int Type;
+	public readonly string? String;
+
+	public Token(Location location, int type): this() {
+		Location = location;
+		Type = type;
+	}
+}
