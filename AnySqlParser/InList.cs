f8a37e2e56f320105b3a6e9ad63801a9bb31f8bf
Russell Wallace <russell.wallace@gmail.com>
2023-12-07 11:46:53 +0000

    IN

---
+++ AnySqlParser/InList.cs
@@ -0,0 +1,12 @@
+﻿namespace AnySqlParser;
+public sealed class InList: Expression {
+	public bool Not;
+	public Expression Left;
+	public List<Expression> Right;
+
+	public InList(bool not, Expression left, List<Expression> right) {
+		Not = not;
+		Left = left;
+		Right = right;
+	}
+}
