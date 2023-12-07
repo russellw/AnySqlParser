49739489be7c0ccc38c56040aceeceb9456c31a0
Russell Wallace <russell.wallace@gmail.com>
2023-12-07 12:24:56 +0000

    test

--- ConsoleApp1/test1.bat
+++ ConsoleApp1/test1.bat
@@ -1,4 +1,3 @@
 cd \AnySqlParser\ConsoleApp1||exit /b
 msbuild /p:Configuration=Debug /p:Platform="Any CPU"||exit /b
-echo off
-"C:\AnySqlParser\ConsoleApp1\bin\Any CPU\Debug\net7.0\ConsoleApp1.exe" "\AnySqlParser\TestProject1\sql-server\cities.sql"
+"C:\AnySqlParser\ConsoleApp1\bin\Any CPU\Debug\net7.0\ConsoleApp1.exe" %1
