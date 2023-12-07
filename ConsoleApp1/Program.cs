3a03e89eebfac9487fd5bd2fb626cf2ae09f9eb6
Russell Wallace <russell.wallace@gmail.com>
2023-12-07 12:06:08 +0000

    fmt

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,12 @@
 ﻿using AnySqlParser;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        foreach (var file in args)
-        {
-            Console.WriteLine(file);
-            foreach(var a in Parser.Parse(file))
-            {
-                Console.WriteLine(a);
-            }
-        }
-        }
-    }
+class Program {
+	static void Main(string[] args) {
+		foreach (var file in args) {
+			Console.WriteLine(file);
+			foreach (var a in Parser.Parse(file)) {
+				Console.WriteLine(a);
+			}
+		}
+	}
+}
