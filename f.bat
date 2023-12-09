2357038daa5330a773201c30355eb7bc84d50bac
Russell Wallace <russell.wallace@gmail.com>
2023-12-09 05:51:49 +0000

    benchmark

--- f.bat
+++ f.bat
@@ -2,5 +2,6 @@
 clang-format -i --style=file AnySqlParser\*.cs||exit /b
 clang-format -i --style=file TestProject1\*.cs||exit /b
 clang-format -i --style=file ConsoleApp1\*.cs||exit /b
+clang-format -i --style=file Benchmark\*.cs||exit /b
 call clean-cs -i -r .||exit /b
 git diff
