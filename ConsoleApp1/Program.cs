1a20ae8427e92445e74ee810b4813ff585edeb45
Russell Wallace <russell.wallace@gmail.com>
2023-12-09 03:34:57 +0000

    color

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ class Program {
 		foreach (var file in args)
 			foreach (var a in Parser.Parse(file)) {
 				if (a is ExtraText extraText) {
-					Console.ForegroundColor = ConsoleColor.DarkYellow;
+					Console.ForegroundColor = ConsoleColor.Red;
 					Console.WriteLine(extraText.Location);
 					Console.ForegroundColor = ConsoleColor.Yellow;
 					Console.WriteLine(extraText.Text);
