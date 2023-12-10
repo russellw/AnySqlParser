ae76b2ca180ce1f26b7fb971b025649c3e7f9150
Russell Wallace <russell.wallace@gmail.com>
2023-12-10 15:05:23 +0000

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program {
 	static void Main(string[] args) {
 		var schema = new Schema();
 		foreach (var file in args)
-			foreach (var a in Parser.Parse(file, schema)) {
+			foreach (var _ in Parser.Parse(file, schema)) {
 			}
 		Console.Write(SqlServerComposer.Compose(schema));
 	}
