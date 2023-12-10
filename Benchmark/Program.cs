048ff9727e246c45a5684993bfdcd6d9af4c59fd
Russell Wallace <russell.wallace@gmail.com>
2023-12-10 15:31:36 +0000

    update

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -6,9 +6,11 @@ class Program {
 		var stopwatch = Stopwatch.StartNew();
 		int n = 0;
 		for (int i = 0; i < 200; i++)
-			foreach (var file in args)
-				foreach (var a in Parser.Parse(file))
+			foreach (var file in args) {
+				var schema = new Schema();
+				foreach (var _ in Parser.Parse(file, schema))
 					n++;
+			}
 		Console.WriteLine(n);
 		Console.WriteLine(stopwatch.Elapsed);
 	}
--- Benchmark/test.bat
+++
@@ -1,8 +0,0 @@
-cd \AnySqlParser\Benchmark||exit /b
-msbuild /p:Configuration=Debug /p:Platform="Any CPU"||exit /b
-echo off
-for %%a in (\AnySqlParser\TestProject1\sql-server\*.sql) do "C:\AnySqlParser\Benchmark\bin\Any CPU\Debug\net7.0\Benchmark.exe" "%%a"||exit /b
-for %%a in (\AnySqlParser\TestProject1\sql-server-samples\*.sql) do "C:\AnySqlParser\Benchmark\bin\Any CPU\Debug\net7.0\Benchmark.exe" "%%a"||exit /b
-for %%a in (\AnySqlParser\TestProject1\northwind_psql\*.sql) do "C:\AnySqlParser\Benchmark\bin\Any CPU\Debug\net7.0\Benchmark.exe" "%%a"||exit /b
-for %%a in (\AnySqlParser\TestProject1\mysql\*.sql) do "C:\AnySqlParser\Benchmark\bin\Any CPU\Debug\net7.0\Benchmark.exe" "%%a"||exit /b
-for %%a in (\AnySqlParser\TestProject1\mysql-samples\*.sql) do "C:\AnySqlParser\Benchmark\bin\Any CPU\Debug\net7.0\Benchmark.exe" "%%a"||exit /b
