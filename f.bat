00e69e902e2a179242e4f43943704d3ada5ecf8f
Russell Wallace <russell.wallace@gmail.com>
2023-12-11 14:31:58 +0000

    round-trip parse/compose

--- f.bat
+++ f.bat
@@ -3,5 +3,6 @@ clang-format -i --style=file AnySqlParser\*.cs||exit /b
 clang-format -i --style=file TestProject1\*.cs||exit /b
 clang-format -i --style=file ConsoleApp1\*.cs||exit /b
 clang-format -i --style=file Benchmark\*.cs||exit /b
+clang-format -i --style=file RoundTrip\*.cs||exit /b
 call clean-cs -i -r .||exit /b
 git diff
