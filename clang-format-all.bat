d2703920810ab4afbd62243f670fdadc81282b90
Russell Wallace <russell.wallace@gmail.com>
2023-12-15 11:38:25 +0000

    clang-format twice

---
+++ clang-format-all.bat
@@ -0,0 +1,5 @@
+clang-format -i --style=file AnySqlParser\*.cs||exit /b
+clang-format -i --style=file TestProject1\*.cs||exit /b
+clang-format -i --style=file ConsoleApp1\*.cs||exit /b
+clang-format -i --style=file Benchmark\*.cs||exit /b
+clang-format -i --style=file RoundTrip\*.cs||exit /b
--- f.bat
+++ f.bat
@@ -1,8 +1,5 @@
 @echo off
-clang-format -i --style=file AnySqlParser\*.cs||exit /b
-clang-format -i --style=file TestProject1\*.cs||exit /b
-clang-format -i --style=file ConsoleApp1\*.cs||exit /b
-clang-format -i --style=file Benchmark\*.cs||exit /b
-clang-format -i --style=file RoundTrip\*.cs||exit /b
+call clang-format-all
 call clean-cs -i -r .||exit /b
+call clang-format-all
 git diff
